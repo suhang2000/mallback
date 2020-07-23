ed0d145725ffc07943cedb93694707e5baa86cbc
胡文哲 <1572065111@qq.com>
2020-07-23 15:22:46 +0800

    注释了一点代码

--- src/main/java/scu/suncaper/mallback/service/OrderService.java
+++ src/main/java/scu/suncaper/mallback/service/OrderService.java
@@ -1,10 +1,7 @@
 package scu.suncaper.mallback.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import scu.suncaper.mallback.dao.OrderDAO;
 import scu.suncaper.mallback.dao.ProductDAO;
 import scu.suncaper.mallback.dao.UserDAO;
@@ -19,11 +16,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static junit.framework.TestCase.assertEquals;
 
 
 @Service
-@RunWith(SpringJUnit4ClassRunner.class)
 public class OrderService {
     @Autowired
     UserDAO userDAO;
@@ -40,11 +35,8 @@ public class OrderService {
 //    }
 
     //查询卖家售卖的所有订单
-    @Test
     public List<Object[]> getAllOrders(Integer sid) {
         Integer defaultSip = 123;
-        assertEquals(productDAO.findAll().size(), 5);
-        assertEquals(userDAO.findAll().size(), 2);
 
         List<Object[]> products = productDAO.findByNameContaining(defaultSip);
         for (Object[] pro : products) {
